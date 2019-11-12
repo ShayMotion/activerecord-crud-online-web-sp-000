f3a9bdfc080c1914ae72d2327f62037149de11ad
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-11-12 15:43:39 +0000

    Automatically backed up by Learn

---
+++ db/migrate/20191108023854_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    add_column Movie do |t|
+      t.string :title
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
+    end
+  end
+end
